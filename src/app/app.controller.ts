dc4cc9234ce84f0a56f821d08ee5a55bd11c627d
daniel.moisa <daniel.moisa@fortech.ro>
2021-11-02 19:23:54 +0200

    remove top level user controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,13 +1,12 @@
 import { controller, IAppController } from '@foal/core';
 import { createConnection } from 'typeorm';
 
-import { ApiController, OpenapiController, UsersController } from './controllers';
+import { ApiController, OpenapiController } from './controllers';
 
 export class AppController implements IAppController {
   subControllers = [
     controller('/api', ApiController),
     controller('/swagger', OpenapiController),
-    controller('/user', UsersController)
   ];
 
   async init() {
